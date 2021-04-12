9e47cd4a7f1877c95e42b58a4134cd55300c203b
nespinoza <nespinoza@stsci.edu>
2021-04-12 16:37:02 -0400

    Works, but trusts on proposer target naming, and doesnt handle spaces in target name.

--- list_targets.py
+++ list_targets.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pandas as pd
 import requests
 
@@ -7,39 +8,75 @@ from bs4 import BeautifulSoup
 r = requests.get("https://www.stsci.edu/jwst/science-execution/approved-programs/cycle-1-go")
 
 # Soup-ify the webpage data:
-soup = BeautifulSoup(r.content)
+soup = BeautifulSoup(r.content, features="lxml")
 
-# Extract tables for all proposals (scraping of tables courtesy of Sean Lockwood at STScI):
+# Extract tables for all proposals on the Exoplanets & Disks category (scraping of tables courtesy of Sean Lockwood at STScI; 
+# modified to only get the first table --- the one for Exoplanets and Disks):
 tables = []
 for table in soup.findAll("table"):
     if table.findParent("table") is None:
         tables.append(pd.read_html(str(table))[0])
+    break
 df = pd.concat(tables)
 
-# Now, using this table, scrape targets using the program information webpage. Save target name and information in a dictionary:
-pid_info = {}
+# Now, using this table, scrape targets using the program information. We save all the data in a dictionary; for each target, we save all the 
+# observations for it:
+target_info = {}
 for program_id in df['ID']:
 
+    print('Scraping '+str(program_id)+'...')
     r_pid = requests.get("https://www.stsci.edu/cgi-bin/get-visit-status?id="+str(program_id)+"&markupFormat=html&observatory=JWST&pi=1")
-    pid_soup = BeautifulSoup(r_pid.content)
+    pid_soup = BeautifulSoup(r_pid.content, features="lxml")
 
-    # Extract target info:
-    tables = []
-    for table in pid_soup.findAll("table"):
-        if table.findParent("table") is None:
-            tables.append(pd.read_html(str(table))[0])
-    pid_df = pd.concat(tables)
+    # Extract target info if availalbe (theory proposal will fail, so skip them):
+    try:
+        tables = []
+        for table in pid_soup.findAll("table"):
+            if table.findParent("table") is None:
+                tables.append(pd.read_html(str(table))[0])
+        pid_df = pd.concat(tables)
 
-    # Iterate through targets; save info to dictionary:
-    pid_info[program_id] = {}
-    for i in range(1, len(pid_df)):
-        if pid_df[3][i] not in pid_info[program_id].keys():
-            pid_info[program_id][pid_df[3][i]] = {}
-            pid_info[program_id][pid_df[3][i]]['Templates'] = np.array([pid_df[4][i]])
-            pid_info[program_id][pid_df[3][i]]['Hours'] = np.array([pid_df[5][i]])
-        else:
-            pid_info[program_id][pid_df[3][i]]['Template'] = np.append(pid_info[program_id][pid_df[3][i]]['Template'], pid_df[4][i])
-            pid_info[program_id][pid_df[3][i]]['Hours'] = np.append(pid_info[program_id][pid_df[3][i]]['Hours'], pid_df[5][i])
+        # Save also info from the current PID:
+        current_pid = df['ID'] == program_id
+        program_title = df[current_pid]['Program Title'].values[0]
+        program_pi = df[current_pid]['PI & Co-PIs'].values[0]
+
+        # Iterate through targets; save info to dictionary:
+        for i in range(1, len(pid_df)):
+
+            # Extract information for current program/targets:
+            target_name, instrument, hours = pid_df[3][i], pid_df[4][i], pid_df[5][i] 
+
+            # Create dict for target if not create already:
+            if target_name not in target_info.keys():
+                target_info[target_name] = {}
+                target_info[target_name]['Templates'] = np.array([])
+                target_info[target_name]['Hours'] = np.array([])
+                target_info[target_name]['PIDs'] = np.array([])
+                target_info[target_name]['PIs'] = np.array([])
+                target_info[target_name]['Titles'] = np.array([])
+
+            # Save info for target:
+            target_info[target_name]['Templates'] = np.append(target_info[target_name]['Templates'], instrument)
+            target_info[target_name]['Hours'] = np.append(target_info[target_name]['Hours'], hours)
+            target_info[target_name]['PIDs'] = np.append(target_info[target_name]['PIDs'], program_id)
+            target_info[target_name]['Titles'] = np.append(target_info[target_name]['Titles'], program_title)
+            target_info[target_name]['PIs'] = np.append(target_info[target_name]['PIs'], program_pi)
+    except:
+        print('Program ID',program_id,'target extraction failed. Probably theory proposal.')
 
 # Now print all the info in comma-separated machine readable form:
-
+fout = open('targets_cycle1.csv', 'w')
+# Write headers out:
+fout.write('Target Name, Instrument Mode, Hours on Mode, Program ID, Program PIs, Program Title\n')
+for target in target_info.keys():
+    all_data = target_info[target]
+    for i in range(len(all_data['Templates'])):
+        print(target)
+        out_string = target+','
+        for info in ['Templates', 'Hours', 'PIDs', 'PIs', 'Titles']:
+            out_string += str(all_data[info][i])+','
+        out_string = out_string[:-1]
+        out_string += '\n'
+        fout.write(out_string)
+fout.close()
